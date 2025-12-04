35995cfb9c08845142f9960bd691c34c22e55269
Matheus Biesek <md.biesek@gmail.com>
2025-12-04 18:14:04 -0300

    chore(services): remove duplicate root-level service files; keep Services/v1 and Services/v2

--- Guardian/Services/IHealthServiceV1.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using Guardian.Models;
-
-namespace Guardian.Services;
-
-public interface IHealthServiceV1
-{
-    Task<HealthResponse> GetHealthAsync();
-}
--- Guardian/Services/IHealthServiceV2.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using Guardian.Models;
-
-namespace Guardian.Services;
-
-public interface IHealthServiceV2
-{
-    Task<HealthResponse> GetHealthAsync();
-}
